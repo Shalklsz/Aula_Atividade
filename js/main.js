4609ae2bae469e52830aaffacd1aefb36564a6dc
unknown <jefferson_carradore1@saojose.sc.senai.br>
2019-06-19 12:37:55 -0300

    término do exercício

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ var num2 = parseInt(prompt("Digite o segundo valor"));
 var result = 0;
 
 var equ = prompt("Selecione a operação: a=+, b=-, c=*, d=/");
+
+if (equ == a) {
+    result = num1 + num2;
+    alert(result);    
+} else if (equ == b) {
+    result = num1 - num2;
+    alert(result);
+}else if (equ == c) {
+    result = num1 * num2;
+    alert(result);    
+}else if (equ == d) {
+    result = num1/num2;
+    alert(result);    
+}
