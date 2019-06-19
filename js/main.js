564a4a6e43c477ba665d2da55f171ebbd464cf24
unknown <jefferson_carradore1@saojose.sc.senai.br>
2019-06-19 12:35:22 -0300

    criação de variáveis e puxar script da pasta ./js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var num1 = parseInt(prompt("Digite o primeiro valor"));
+var num2 = parseInt(prompt("Digite o segundo valor"));
+var result = 0;
+
+var equ = prompt("Selecione a operação: a=+, b=-, c=*, d=/");
